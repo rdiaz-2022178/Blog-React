ed23de4576f23550102d69db9f5c366314c27ae9
rdiaz-2022178 <rdiaz-2022178@kinal.edu.gt>
2024-05-10 08:33:22 -0600

    se agregan post de proyectos

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -5,6 +5,11 @@ import imgCoperex from '../assets/Coperex.png';
 import imgTienda from '../assets/Tienda.png';
 import imgKinal from '../assets/Kinal.png';
 import imgTonys from '../assets/Tonys.png';
+import imgInfografiaDe from '../assets/InfografiaDe.png';
+import imgCOnceptualTec from '../assets/ConceptualTecno.png';
+import imgPrueba1 from '../assets/Prueba1.png';
+import imgMapaMental from '../assets/MapaMental.png';
+import imgKahoot from '../assets/Kahoot.png';
 
 const Proyectos = () => {
   const proyectos = [
@@ -50,6 +55,51 @@ const Proyectos = () => {
       description: 'Proyecto Hecho en java EEpara tonys kinal',
       link: {
         url: 'https://github.com/rdiaz-2022178/ProyectoExpo2023.git',
+        label: 'Ver más'
+      }
+    },
+    {
+      image: imgCOnceptualTec,
+      title: 'Infografia Desarrollo Web',
+      description: 'Infografia explicando  el desarrollo web',
+      link: {
+        url: 'https://www.canva.com/design/DAF6SuD6Rw4/VNBwvfNjhQxMTJkDd34ekg/edit?utm_content=DAF6SuD6Rw4&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton',
+        label: 'Ver más'
+      }
+    },
+    {
+      image: imgCOnceptualTec,
+      title: 'Mapa conceptual Tecnologias web',
+      description: 'Mapa conceptual donde investigo las tecnologias para el desarrollo web-',
+      link: {
+        url: 'https://www.canva.com/design/DAF7eguslsk/co4WB5Gd0sGF43fpx-6uaQ/view?utm_content=DAF7eguslsk&utm_campaign=designshare&utm_medium=link&utm_source=editor',
+        label: 'Ver más'
+      }
+    },
+    {
+      image: imgPrueba1,
+      title: 'Prueba objetiva 1',
+      description: ' Se realizo una prueba objetiva del los temas del desarrollo web',
+      link: {
+        url: 'https://kinal.academy/mod/quiz/view.php?id=378764',
+        label: 'Ver más'
+      }
+    },
+    {
+      image: imgMapaMental,
+      title: 'Mapa mental sobre el desarrollo we',
+      description: 'sobre el desarrollo web',
+      link: {
+        url: 'https://www.canva.com/design/DAF8tawz2x4/JOytCyB3XEtbYNlID_9GrA/view?utm_content=DAF8tawz2x4&utm_campaign=designshare&utm_medium=link&utm_source=editor',
+        label: 'Ver más'
+      }
+    },
+    {
+      image: imgKahoot,
+      title: 'Kahoot',
+      description: 'Kahoot sobre temas vistos en clase',
+      link: {
+        url: 'https://kinal.academy/mod/assign/view.php?id=416903',
         label: 'Ver más'
       }
     },
